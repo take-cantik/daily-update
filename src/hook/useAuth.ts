2fbc2d68b99a24ee1642374776ee94799def6a98
take-cantik <take.cantik17@gmail.com>
2022-02-16 20:56:03 +0900

    Add: useAuth hook

---
+++ src/hook/useAuth.ts
@@ -0,0 +1,41 @@
+import {
+  GithubAuthProvider,
+  linkWithPopup,
+  signInWithPopup,
+  signOut,
+  TwitterAuthProvider,
+  UserCredential
+} from "firebase/auth";
+import { useRecoilState } from "recoil";
+import { firebaseAuth } from "~/infra/firebase";
+import { authState } from "~/state/auth";
+
+export const useAuth = () => {
+  const githubLogin = async (): Promise<UserCredential> => {
+    const provider = new GithubAuthProvider();
+    return await signInWithPopup(firebaseAuth, provider);
+  };
+
+  const twitterLink = async (): Promise<void> => {
+    const provider = new TwitterAuthProvider();
+    if (firebaseAuth.currentUser) {
+      const credential: UserCredential = await linkWithPopup(
+        firebaseAuth.currentUser,
+        provider
+      );
+      const user = credential.user;
+      console.log(user);
+    }
+  };
+
+  const logout = (): Promise<void> => {
+    return signOut(firebaseAuth);
+  };
+
+  return {
+    auth: useRecoilState(authState),
+    githubLogin,
+    twitterLink,
+    logout
+  };
+};
