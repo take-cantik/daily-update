2fef0a3849164026a9143318e44e8b1dfcc75675
take-cantik <take.cantik17@gmail.com>
2022-02-16 21:34:17 +0900

    Test: login and link account

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { Fragment } from "react";
+import { useAuth } from "~/hook/useAuth";
 
 const Home: NextPage = (): JSX.Element => {
-  return <h1>Hello, world!</h1>;
+  const { githubLogin, twitterLink, logout } = useAuth();
+
+  return (
+    <Fragment>
+      <h1>Hello, world!</h1>
+      <button onClick={githubLogin}>github</button>
+      <button onClick={twitterLink}>twitter</button>
+      <button onClick={logout}>logout</button>
+    </Fragment>
+  );
 };
 
 export default Home;
