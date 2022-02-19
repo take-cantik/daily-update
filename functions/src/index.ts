78a1fa27f5586012791b40bf5c3311a6f7627df5
take-cantik <take.cantik17@gmail.com>
2022-02-20 03:59:17 +0900

    Add: firebase admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,46 @@
-import * as functions from "firebase-functions";
+import { credential, firestore, initializeApp } from "firebase-admin";
+import { https } from "firebase-functions";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export interface CurrentUser {
+  id: string;
+  uid: string;
+  githubId: string;
+  githubAvatarUrl: string;
+  dailyContributions: number[];
+  totalContributions: number;
+  version: number;
+  twitterId?: string;
+  twitterAvatarUrl?: string;
+}
+
+const app = initializeApp({
+  credential: credential.applicationDefault(),
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DETABASE_URL
+});
+const db = firestore(app);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
+// export const helloWorld = https.onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// export const dailyUpdate = pubsub
+//   .schedule("every 1 days")
+//   .onRun(async () => {
+//     await db.collection("users").get();
+//   });
+
+export const dailyUpdate = https.onRequest(
+  async (request: NextApiRequest, response: NextApiResponse) => {
+    const users: CurrentUser[] = [];
+    const res = await db.collection("users").get();
+    res.forEach((doc) => {
+      users.push(doc.data() as CurrentUser);
+    });
+    response.send(users);
+  }
+);
