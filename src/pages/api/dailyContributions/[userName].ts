656cfa74eb79978e786ac668ce3acd299f7bd2d7
take-cantik <take.cantik17@gmail.com>
2022-02-19 11:21:36 +0900

    Add: query

--- src/pages/api/dailyContributions/[userName].ts
+++ src/pages/api/dailyContributions/[userName].ts
@@ -1,2 +1,68 @@
 // 毎日の更新用
-export default async function handler() {}
+
+import { Octokit } from "@octokit/core";
+import dayjs from "dayjs";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Contributions = {
+  user: {
+    contributionsCollection: {
+      contributionCalendar: {
+        weeks: [
+          {
+            contributionDays: [
+              {
+                date: string;
+                contributionCount: number;
+              }
+            ];
+          }
+        ];
+      };
+    };
+  };
+};
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const { userName } = request.query;
+
+  const octokit = new Octokit({
+    auth: process.env.NEXT_PUBLIC_GITHUB_API_KEY
+  });
+
+  const now = dayjs().format("YYYY-MM-DDThh:mm:ss");
+  const oneWeekBefore = dayjs()
+    .subtract(1, "week")
+    .format("YYYY-MM-DDThh:mm:ss");
+
+  const query = `
+    query contributions ($userName:String!, $now:DateTime!, $oneWeekBefore:DateTime!) {
+      user(login: $userName) {
+        contributionsCollection(to: $now, from: $oneWeekBefore) {
+          contributionCalendar {
+            weeks {
+              contributionDays {
+                date
+                contributionCount
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const contributions = await octokit.graphql<Contributions>(query, {
+    userName,
+    now,
+    oneWeekBefore
+  });
+
+  // code
+  console.log(contributions);
+
+  return response.status(200).json({});
+}
--- src/pages/api/totalContributions/[userName].ts
+++ src/pages/api/totalContributions/[userName].ts
@@ -1,2 +1,46 @@
 // 初回に実行する用
-export default async function handler() {}
+
+import { Octokit } from "@octokit/core";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Contributions = {
+  user: {
+    contributionsCollection: {
+      contributionCalendar: {
+        totalContributions: number;
+      };
+    };
+  };
+};
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const { userName } = request.query;
+
+  const octokit = new Octokit({
+    auth: process.env.NEXT_PUBLIC_GITHUB_API_KEY
+  });
+
+  const query = `
+    query contributions ($userName:String!) {
+      user(login: $userName) {
+        contributionsCollection {
+          contributionCalendar {
+            totalContributions
+          }
+        }
+      }
+    }
+  `;
+
+  const contributions = await octokit.graphql<Contributions>(query, {
+    userName
+  });
+
+  // code
+  console.log(contributions);
+
+  return response.status(200).json({});
+}
