7c5c6030fdcaa347c5c0b017fc9118d43e59432d
take-cantik <take.cantik17@gmail.com>
2022-02-21 15:44:52 +0900

    Update: use Buffer

--- functions/src/api/contribution.ts
+++ functions/src/api/contribution.ts
@@ -1,13 +1,15 @@
 import { config } from "firebase-functions";
+import { Buffer } from "buffer";
 import fetch from "node-fetch";
 import { baseUrl } from "..";
 // import { DaliyContributions, TotalContributions } from "../types";
 
 export const getContributinos = async (githubId: string) => {
+  const userName = Buffer.from(config().basic.username).toString("base64");
+  const password = Buffer.from(config().basic.password).toString("base64");
+
   const headers = {
-    Authorization: `Basic ${btoa(config().basic.username as string)}:${btoa(
-      config().basic.password as string
-    )}`
+    Authorization: `Basic ${userName}:${password}`
   };
 
   const responses = await Promise.all([
